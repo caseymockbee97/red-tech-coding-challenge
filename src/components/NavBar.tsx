e52a4ce4904fc3c2ff23449bad4e5b479a180308
Casey Mockbee <caseymockbee97@gmail.com>
2024-03-06 21:15:33 -0500

    Added a navbar and layout.

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import { AccountCircle, Settings } from '@mui/icons-material';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import logo from '../assets/logo.png';
+
+export const NavBar = () => {
+  return (
+    <AppBar color="transparent">
+      <Toolbar>
+        <InnerContainer $gap="12px">
+          <Image src={logo} />
+          <Typography variant="h5">Home</Typography>
+        </InnerContainer>
+        <Box sx={{ flexGrow: 1 }} />
+        <InnerContainer $gap="0px">
+          <IconButton>
+            <Settings />
+          </IconButton>
+          <IconButton>
+            <AccountCircle />
+          </IconButton>
+        </InnerContainer>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+const InnerContainer = styled.div<{ $gap: string }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${({ $gap }) => $gap};
+`;
+
+const Image = styled.img`
+  height: 2.5rem;
+`;
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import { NavBar } from '../components';
+import { Toolbar } from '@mui/material';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <Container>
+      <NavBar />
+      {/* Adding the empty toolbar prevents the Navbar from covering content. */}
+      <Toolbar />
+      {children}
+    </Container>
+  );
+};
+
+const Container = styled.div``;
